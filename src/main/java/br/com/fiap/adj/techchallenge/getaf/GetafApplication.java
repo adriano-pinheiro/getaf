5207b5d604f6f67c25cca7ba0b018933a63f6713
Adriano Pinheiro <adriano_fiorini@hotmail.com>
2024-05-05 15:46:27 -0300

    Versão inicial

---
+++ src/main/java/br/com/fiap/adj/techchallenge/getaf/GetafApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.adj.techchallenge.getaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetafApplication.class, args);
+	}
+
+}
