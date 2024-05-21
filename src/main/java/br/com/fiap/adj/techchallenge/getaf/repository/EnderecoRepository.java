4c69fed58fcda8db7ccdf29722e48b80a67ed205
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2024-05-21 16:45:27 -0300

    Estrutura de cadastro de clientes

---
+++ src/main/java/br/com/fiap/adj/techchallenge/getaf/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.adj.techchallenge.getaf.repository;
+
+import br.com.fiap.adj.techchallenge.getaf.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
