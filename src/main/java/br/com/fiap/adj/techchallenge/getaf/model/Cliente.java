8bf5d841ad3fd9e92fa03a2e5ef268fc13a9e44f
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2024-05-21 16:44:36 -0300

    Estrutura de cadastro de clientes

--- src/main/java/br/com/fiap/adj/techchallenge/getaf/model/Cliente.java
+++ src/main/java/br/com/fiap/adj/techchallenge/getaf/model/Cliente.java
@@ -3,6 +3,7 @@ package br.com.fiap.adj.techchallenge.getaf.model;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_clientes")
@@ -16,52 +17,34 @@ public class Cliente implements Serializable {
 
     private String nome;
 
-    @Column(unique = true)
     private String cnpj;
 
-    @Column(unique = true)
     private String email;
 
     private String telefone;
 
-    private String endereco;
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Endereco> enderecos;
 
-    private String complemento;
-
-    private String cep;
-
-    private String bairro;
-
-    private String cidade;
-
-    private String uf;
-
-    public Cliente(Long id,
-                   String nome,
-                   String cnpj,
-                   String email,
-                   String telefone,
-                   String endereco,
-                   String complemento,
-                   String cep,
-                   String bairro,
-                   String cidade,
-                   String uf) {
+    public Cliente(Long id, String nome, String cnpj, String email, String telefone, List<Endereco> enderecos) {
         this.id = id;
         this.nome = nome;
         this.cnpj = cnpj;
         this.email = email;
         this.telefone = telefone;
-        this.endereco = endereco;
-        this.complemento = complemento;
-        this.cep = cep;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.uf = uf;
+        this.enderecos = enderecos;
     }
 
     public Cliente() {
 
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
     }
 
     public Long getId() {
@@ -102,54 +85,6 @@ public class Cliente implements Serializable {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
     }
 
     @Override
