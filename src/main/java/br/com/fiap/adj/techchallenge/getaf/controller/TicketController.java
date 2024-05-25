065805fdfba991c4fec3f39ac6e511efe82f13da
Vinicius Quadros <voa.quadros@gmail.com>
2024-05-25 13:44:59 -0300

    implementado paginação no método getAllTickets e atualizado collection de Tickets do Postman

--- src/main/java/br/com/fiap/adj/techchallenge/getaf/controller/TicketController.java
+++ src/main/java/br/com/fiap/adj/techchallenge/getaf/controller/TicketController.java
@@ -7,6 +7,9 @@ import br.com.fiap.adj.techchallenge.getaf.service.impl.TicketServiceImpl;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -36,8 +38,9 @@ public class TicketController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Ticket>> getAllTickets(){
-        return ResponseEntity.status(HttpStatus.OK).body(service.getTickets());
+    public ResponseEntity<Page<Ticket>> getAllTickets(
+            @PageableDefault(page = 0, size = 10, sort = "nome") Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getTickets(pageable));
     }
 
     @GetMapping("/{id}")
--- src/main/java/br/com/fiap/adj/techchallenge/getaf/service/TicketService.java
+++ src/main/java/br/com/fiap/adj/techchallenge/getaf/service/TicketService.java
@@ -4,12 +4,13 @@ import br.com.fiap.adj.techchallenge.getaf.dto.TicketDTO;
 import br.com.fiap.adj.techchallenge.getaf.dto.UpdateTicketDTO;
 import br.com.fiap.adj.techchallenge.getaf.model.Ticket;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TicketService {
-    List<Ticket> getTickets();
+    Page<Ticket> getTickets(Pageable pageable);
     Optional<Ticket> getTicket(Long id);
     void deleteTicket(Long id);
     Ticket createTicket(TicketDTO ticket);
